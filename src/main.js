f737e470b1683f109de61cb6e2d86b0d24167a9f
colemilne54 <colemilne54@gmail.com>
2023-03-22 10:32:57 -0600

    3 files modified

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 const { createApp } = require('vue');
-import App from "./App.vue";
+import App from './App.vue';
+import Vue from 'vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-createApp(App).mount("#app");
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+
+createApp(App).mount('#app');
